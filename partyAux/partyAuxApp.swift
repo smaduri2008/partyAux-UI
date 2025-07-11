3c04401f49df0f6f1f6a07722c62e6c1ccf0155c
sahas <sahas.maduri@gmail.com>
2025-07-11 12:42:39 -0400

    Initial Commit

---
+++ partyAux/partyAuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  partyAuxApp.swift
+//  partyAux
+//
+//  Created by Sahas Maduri on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct partyAuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ partyAuxTests/partyAuxTests.swift
@@ -0,0 +1,17 @@
+//
+//  partyAuxTests.swift
+//  partyAuxTests
+//
+//  Created by Sahas Maduri on 7/11/25.
+//
+
+import Testing
+@testable import partyAux
+
+struct partyAuxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
